80a9c9e894f192590e98a457267aca4e8dff7d0f
king0120 <king0120@gmail.com>
2018-03-27 12:38:16 -0400

    inclass

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/artists', to: 'artists#index'
-  get '/artists/new', to: 'artists#new'
-  post '/artists', to: 'artists#create'
-  get '/artists/:id', to: 'artists#show'
-  get '/artists/:id/edit', to: 'artists#edit'
-  put '/artists/:id', to: 'artists#update'
-  delete '/artists/:id', to: 'artists#destroy'
+  resources :artists
 end
